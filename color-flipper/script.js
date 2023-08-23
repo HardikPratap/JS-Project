72dcfacb7bffefd3561e9973a228a7532547beac
Hardik Pratap <hardikpratap188@gmail.com>
2023-08-23 18:05:01 +0530

    implemented js

--- color-flipper/script.js
+++ color-flipper/script.js
@@ -1,1 +1,16 @@
 const colors=["green" ,"red","rgba(133,122,200", "#f15025"]
+
+const btn= document.getElementById("btn");
+const color=document.getElementById("color");
+
+btn.addEventListener("click", function(){
+    const random= getRandom();
+    document.body.style.backgroundColor= colors[random];
+    document.getElementById("color").textContent=colors[random];
+    //alt
+    //document.querySelector(".color").textContent=color[randomColor];
+})
+
+function getRandom(){
+    return Math.floor(Math.random() * colors.length) ;
+}
