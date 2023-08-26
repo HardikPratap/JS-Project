e72cbf5ea03e0faf450ecfae3d3df4043f6cc926
Hardik Pratap <hardikpratap188@gmail.com>
2023-08-26 18:41:56 +0530

    added css

---
+++ counter/script.js
@@ -0,0 +1,10 @@
+let count=0;
+
+const value=document.querySelector('#value');
+const btns=document.querySelectorAll('.btn');
+
+btns.forEach(function(item) {
+    item.addEventListener('click', function(e){
+        
+    })
+});
