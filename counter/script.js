26089a10a9cb6861ab4d8e8fa5d65d6a4b9b241a
Hardik Pratap <hardikpratap188@gmail.com>
2023-08-26 18:52:37 +0530

    JS completed

--- counter/script.js
+++ counter/script.js
@@ -1,3 +1,4 @@
+
 let count=0;
 
 const value=document.querySelector('#value');
@@ -5,6 +6,26 @@ const btns=document.querySelectorAll('.btn');
 
 btns.forEach(function(item) {
     item.addEventListener('click', function(e){
-        
+        const styles=e.currentTarget.classList;
+        // console.log(styles);
+        if(styles.contains('decrease')){
+            count--;
+        }
+        else if(styles.contains('reset')){
+            count=0;
+        }
+        else {
+            count++;
+        }
+        if(count>0){
+            value.style.color='Green';
+        }
+        if(count<0){
+            value.style.color='Red';
+        }
+        if(count===0){
+            value.style.color='Black';
+        }
+        value.textContent=count;
     })
 });
